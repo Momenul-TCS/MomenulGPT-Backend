a6cf9c0afd6490e2d16b4cdbafd3bfa1220e298b
Momenul Islam <momenulelectrical@gmail.com>
2025-08-26 09:04:39 +0530

    first commit

---
+++ src/main/java/com/momenul/firstAIdemoProject/FirstAIdemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.momenul.firstAIdemoProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAIdemoProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAIdemoProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/momenul/firstAIdemoProject/FirstAIdemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.momenul.firstAIdemoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAIdemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
